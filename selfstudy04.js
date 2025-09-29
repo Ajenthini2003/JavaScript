a0f834769a9ef35db85d6d3aa44e8341550bd941
Ajenthini2003 <ajenthini.uki.stu@dreamspace.academy>
2025-09-29 21:52:16 +0530

    created selfstudy04.js

---
+++ selfstudy04.js
@@ -0,0 +1,44 @@
+//Math functions
+let c;
+c=Math.PI;
+console.log(c);
+
+let e;
+e=Math.E;
+console.log(e);
+
+d=Math.round(9.6);
+console.log(d);
+
+f=Math.floor(5.3);
+console.log(f);
+
+g=Math.ceil(5.3);
+console.log(g);
+
+h=Math.sqrt(90);
+console.log(h);
+
+i=Math.abs(-80);
+console.log(i);
+
+j=Math.trunc(2.86);
+console.log(j);
+
+k=Math.pow(2,4);
+console.log(k);
+
+l=Math.min(12,14,16,19,17);
+console.log(l);
+
+m=Math.max(12,14,16,19,17);
+console.log(m);
+
+n=Math.random();
+console.log(n);
+
+o=Math.floor(Math.random()*50+1);
+console.log(o);
+
+p=Math.sin(25);
+console.log(p);
